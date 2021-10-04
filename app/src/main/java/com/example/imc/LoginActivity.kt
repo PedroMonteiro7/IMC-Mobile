b7ca4c10bebad50c83595f786b64bb3f20753a35
PedroMonteiro7 <21192977@CFP127ALUNO.LOCAL>
2021-10-04 11:26:21 -0300

    Implementando um menu e redirecionamento de tela

--- app/src/main/java/com/example/imc/LoginActivity.kt
+++ app/src/main/java/com/example/imc/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val criarConta = findViewById<TextView>(R.id.criar_conta)
+
+        criarConta.setOnClickListener {
+            val abrirProfileActivity = Intent(this, ProfileActivity::class.java)
+            //intent chama outras telas e até mesmo outras aplicações;
+
+            startActivity(abrirProfileActivity)
+        }
+
     }
 }
--- app/src/main/java/com/example/imc/ProfileActivity.kt
+++ app/src/main/java/com/example/imc/ProfileActivity.kt
@@ -2,10 +2,34 @@ package com.example.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        supportActionBar!!.title = "Testando menu"
+        supportActionBar!!.subtitle = "Funcionou? Acho que sim, né?!"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_save -> {
+                Toast.makeText(this, "Salvar", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return true
     }
 }
